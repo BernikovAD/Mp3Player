bd9364333776ed4cba875434ee47ad7bd7b2597a
berni <bernikov_1993@mail.ru>
2021-09-06 12:41:23 +0500

    1

---
+++ app/src/main/java/com/example/mp3player/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mp3player.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mp3player.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
